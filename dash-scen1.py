e113bfe77793626e50bd5627df46c25b5d6e869f
kirill <k.nelyubov@gmail.com>
2025-01-04 00:42:34 +0300

    #4-01-25

--- dash-scen1.py
+++ dash-scen1.py
@@ -1,11 +1,14 @@
 from dash import Dash, dcc, html, Input, Output, State, MATCH, ALL, Patch, callback
 import dash_bootstrap_components as dbc
 import json
+import time
 
 with open('config.json') as json_file:
     devices = json.load(json_file)
 
-def if_row_create(n_row):
+def if_row_create(count=[0]):
+    n_row = count[0]
+    count[0] += 1
     return dbc.Row([
         dcc.Dropdown(
             id={
@@ -14,7 +17,6 @@ def if_row_create(n_row):
             },
             options=[{'label': x['name'], 'value': devices.index(x)}
                      for x in devices if 'states' in x],
-#            value=2,
             clearable=False,
             style={'width': '180px'}
         ),
@@ -47,7 +49,9 @@ def if_row_create(n_row):
         )
     ])
 
-def then_todo_create(n_row):
+def then_todo_create(count=[0]):
+    n_row = count[0]
+    count[0] += 1
     return html.Div(
         id={
             'type': 'then-todo-div',
@@ -112,7 +116,6 @@ def then_row_create(n_row):
                 'type': 'then-button-',
                 'index': n_row
             },
-            value='-',
             style={'width': '40px'}
 #            disabled=True
         )
@@ -125,7 +128,7 @@ app.layout = html.Div([
             placeholder='Название сценария',
             id='scene-input',
             debounce=True,
-#            value='Тест',
+            value ='',
             style={'width': '250px'}
         ),
         html.Button("сохранить",
@@ -133,15 +136,21 @@ app.layout = html.Div([
             disabled=True,
             n_clicks=0,
             style={'width': '100px'}
+        ),
+        html.Button("загрузить",
+            id="load-btn",
+#            disabled=True,
+            n_clicks=0,
+            style={'width': '100px'}
         )
     ]),
     html.Div(
         id='if-row-container-div',
-        children=[if_row_create(0)]
+        children=[if_row_create()]
     ),
     html.Div(
         id='then-row-container-div',
-        children=[then_todo_create(0)]
+        children=[then_todo_create()]
     )
 ])
 
@@ -208,13 +217,12 @@ def display_if_todo(value):
 @callback(
     Output('if-row-container-div', 'children'),
     Input({'type': 'if-todo-dropdown', 'index': ALL}, 'value'),
-    [State({'type': 'if-todo-dropdown', 'index': ALL}, 'id'),
-     State('if-row-container-div', 'children')],
+    State('if-row-container-div', 'children'),
     prevent_initial_call=True
 )
-def display_if_container_div(values, ids, children):
+def display_if_container_div(values, children):
     if values[-1] != 'ТОГДА':
-        children.append(if_row_create(ids[-1]['index'] + 1))
+        children.append(if_row_create())
     for i, value in enumerate(values):
         if value == 'удалить':
            del children[i]
@@ -293,40 +301,96 @@ def display_then_value(feature, device, id_):
     [State({'type': 'then-todo-dropdown', 'index': ALL}, 'id'),
      State({'type': 'then-button-', 'index': ALL}, 'id'),
      State('then-row-container-div', 'children')],
-#    prevent_initial_call=True
+    prevent_initial_call=True
 )
 def display_then_container_div(values, del_clicks, add_clicks, ids, ids_row, children):
 
     def del_row(n_row):
-        for row in children:
-            if row['props']['id']['index'] == n_row:
-                children.remove(row)
+        for row_ in children:
+            if row_['props']['id']['index'] == n_row:
+                children.remove(row_)
                 break
 
-    print(add_clicks)
+    if 1 in add_clicks:
+        if ids:
+            del_row(ids[0]['index'])
+        for i, row in enumerate(children):
+            if row['props']['id']['index'] == ids_row[add_clicks.index(1)]['index']:
+                children.insert(i, then_todo_create())
+                break
+        return children
     if 1 in del_clicks:
         del_row(ids_row[del_clicks.index(1)]['index'])
         if not ids:
-            children.append(then_todo_create(ids_row[-1]['index'] + 1))
+            children.append(then_todo_create())
         return children
     if len(values) < len(ids_row):
         return children
     if None in values and ids:
         del_row(ids[0]['index'])
     elif None not in values and not ids:
-        children.append(then_todo_create(ids_row[-1]['index'] + 1))
+        children.append(then_todo_create())
     return children
 
 @callback(
-    Output({'type': 'then-button+', 'index': ALL}, 'disabled'),
-    Input({'type': 'then-value-input', 'index': ALL}, 'value'),
-    State({'type': 'then-button+', 'index': ALL}, 'id'),
+    [Output({'type': 'then-button+', 'index': ALL}, 'disabled'),
+     Output({'type': 'then-button+', 'index': ALL}, 'n_clicks'),
+     Output('save-btn', 'disabled')],
+    [Input({'type': 'then-value-input', 'index': ALL}, 'value'),
+     Input('scene-input', 'value'),
+     Input({'type': 'if-value-input', 'index': ALL}, 'value')],
+    [State({'type': 'then-button+', 'index': ALL}, 'id'),
+     State({'type': 'if-device-dropdown', 'index': ALL}, 'id')],
 #    prevent_initial_call=True
 )
-def display_then_button(values, ids):
-    ret = len(values) < len(ids) or None in values
-    return [ret for i in ids]
+def display_then_button(values, name, if_values, ids, if_ids):
+    ret = len(if_values) and len(if_values) < len(if_ids) or None in if_values
+    ret = not ids or len(values) < len(ids) or None in values or ret
+    return [ret for i in ids], [0 for i in ids], ret or not name
 
+def scene_to_list(scene, rows, then):
+    for row in rows:
+        if then:
+            row = row['props']['children']
+            if type(row) is list:
+                continue
+        for props in row['props']['children']:
+            value = props['props'].get('value', props['props'].get('children'))
+            if value and type(value) is list:
+                value = value[0]['props']['value']
+            scene.append(value)
+
+if_rows_save = []
+then_rows_save = []
+
+@callback(
+#    Output({'type': 'then-button+', 'index': MATCH}, 'n_clicks'),
+    Input('save-btn', 'n_clicks'),
+    [State('scene-input', 'value'),
+     State('if-row-container-div', 'children'),
+     State('then-row-container-div', 'children')]
+#    prevent_initial_call=True
+)
+def press_save_button(n_clicks, name, if_rows, then_rows):
+    global if_rows_save, then_rows_save
+    if_rows_save = if_rows
+    then_rows_save = then_rows
+    scene = [name]
+    scene_to_list(scene, if_rows, False)
+    scene_to_list(scene, then_rows, True)
+#    print(scene)
+#    return 0
+
+@callback(
+    [Output('if-row-container-div', 'children', allow_duplicate=True),
+     Output('then-row-container-div', 'children', allow_duplicate=True)],
+    Input('load-btn', 'n_clicks'),
+    prevent_initial_call=True,
+)
+
+def press_load_button(n_clicks):
+    print(if_rows_save, then_rows_save)
+    return if_rows_save, then_rows_save
 
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=True)
